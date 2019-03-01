c0b73ef4d03ce67b54daef27155a8c52c92e746e
funicia <funicia@163.com>
2019-03-01 18:02:03 +0800

    add order and sigle row color

--- HSJY_JKK_MONITORMAIL.sql
+++ HSJY_JKK_MONITORMAIL.sql
@@ -1,68 +1,116 @@
 
 
 ALTER PROC HSJY_JKK_MONITORMAIL(
-	@TBNAME			NVARCHAR(100),	/*���������ʱ������*/
-	@profile_name	Nvarchar(100),	/*�ʼ������������ļ�����*/
-	@Title			NVARCHAR(200),	/*�ʼ����ı���*/
-	@Mcontent		Nvarchar(2000),	/*�ʼ����ݼ���*/
-	@recipients		NVARCHAR(2000),	/*�ռ��ˣ������;����*/
-	@copy_recipients NVARCHAR(2000) = NULL /*����*/
+	@TBNAME			NVARCHAR(100),	/*监控数据临时表名称*/
+	@profile_name	Nvarchar(100),	/*邮件服务器配置文件名称*/
+	@Title			NVARCHAR(200),	/*邮件正文标题*/
+	@Mcontent		Nvarchar(2000),	/*邮件内容简述*/
+	@recipients		NVARCHAR(2000),	/*收件人，多个用;隔开*/
+	@copy_recipients NVARCHAR(2000) = NULL, /*抄送*/
+	@ORDER_COLUMNS	NVARCHAR(100) = NULL /*排序字段，支持字段名排序，默认按首字段正序排*/
 
 )
 AS
 
 /*
-	�����ݿ���ҵ�ķ�ʽ���м��,�ýű��Ѽ������д����ʱ��,Ȼ������������䲢�������ü��ɽ����ʼ�����.
-	sample: EXEC dbo.HSJY_JKK_MonitorMail 'tempdb..#JKBC','rfc_test','�ʼ���������','funicia@xxx.com','xxx@xxx.com'
+	用数据库作业的方式进行监控,用脚本把监控数据写入临时表,然后调用下面的语句并进行设置即可进行邮件提醒.
+	sample: EXEC dbo.HSJY_JKK_MonitorMail 'tempdb..#JKBC','rfc_test','邮件报错测试','renfc@gildata.com','appdata@gildata.com'
 */
 
---DECLARE @TBNAME			VARCHAR(100),	/*���������ʱ������*/
---		  @profile_name		varchar(100),	/*�ʼ������������ļ�����*/
---		  @@Title			VARCHAR(200),	/*�ʼ����ı���*/
---		  @Mcontent			varchar(2000),	/*�ʼ����ݼ���*/
---		  @recipients		VARCHAR(2000)	/*�ռ��ˣ������';'����*/
- 
-DECLARE @SQLSTR	NVARCHAR(MAX),		
-		@COLUMNS NVARCHAR(MAX) = '',
-		@COLID  INT = 1
-		 
+--DECLARE @TBNAME			VARCHAR(100),	/*监控数据临时表名称*/
+--		  @profile_name		varchar(100),	/*邮件服务器配置文件名称*/
+--		  @@Title			VARCHAR(200),	/*邮件正文标题*/
+--		  @Mcontent			varchar(2000),	/*邮件内容简述*/
+--		  @recipients		VARCHAR(2000),	/*收件人，多个用';'隔开*/
+--		  @copy_recipients NVARCHAR(2000) = NULL, /*抄送*/
+--		  @ORDER_COLUMNS	NVARCHAR(100) = NULL /*排序字段，支持字段名排序，默认按首字段正序排*/
+ 	
 
-WHILE @COLID<=(SELECT MAX(colid) FROM tempdb..syscolumns WHERe id = OBJECT_ID(@TBNAME))
+-----------------------------------------------------------------
+
+/*如果按位置传排序参数，暂时不支持，默认给成第一个字段排序*/
+BEGIN TRY
+	EXEC('SELECT XH = ROW_NUMBER() OVER(ORDER BY '+@ORDER_COLUMNS+') INTO #TMP'+ ' FROM '+@TBNAME)
+END TRY
+BEGIN CATCH
+	SELECT TOP 1 @ORDER_COLUMNS =  '['+ name +']' FROM tempdb..syscolumns WHERe id = OBJECT_ID(@TBNAME)  ORDER BY colid 
+END CATCH
+
+/*字段列表*/
+DECLARE @C NVARCHAR(MAX)
+SELECT @C = STUFF((SELECT ',['+name+']' FROM tempdb..syscolumns WHERe id = OBJECT_ID(@TBNAME) ORDER BY colid FOR XML PATH('')),1,1,'') 
+
+-----------------------------------------------------------------
+
+/*将数据按排序字段排序，将排序后的数据放入tmp_html_id*/
+DECLARE @SQLSS NVARCHAR(MAX)
+SET @SQLSS =
+		  ' INSERT INTO '+@TBNAME +CHAR(10)+
+		  ' SELECT '+@C+', XH = ROW_NUMBER() OVER(ORDER BY '+@ORDER_COLUMNS+')'+ 
+		  ' FROM '+@TBNAME + ' A' +
+		  ' DELETE FROM '+@TBNAME + ' WHERE tmp_html_id IS NULL'
+
+
+IF NOT EXISTS (SELECT 1 FROM tempdb..syscolumns WHERe id = OBJECT_ID(@TBNAME) AND name =  'tmp_html_id')
 BEGIN
-	SELECT @COLUMNS=@COLUMNS+',ISNULL(['+name+'],'+''' '')' +' AS [TD],'+'+'' '+'''' FROM tempdb..syscolumns WHERe id = OBJECT_ID(@TBNAME) and colid=@COLID
-	SELECT @COLID=@COLID+1
+	EXEC('	alter table '+@TBNAME +'
+			add tmp_html_id int')	
+	EXEC (@SQLSS)
 END
-  
-SELECT @COLUMNS=SUBSTRING(@COLUMNS,2,LEN(@COLUMNS)-1)
+
+----------------------------------------------------------------------------------------------------------------
+
+DECLARE @SQLSTR	NVARCHAR(MAX),	
+		@COLUMNS NVARCHAR(MAX) = ''
+SELECT @COLUMNS = 
+(
+	SELECT	  ','+
+			  CASE WHEN xtype = 61 then 'CONVERT(VARCHAR(23),' ELSE '' END + 
+				'ISNULL(['+name+'],'+  --日期格式的字段，对其进行转换，防止出现“日期T时间”的情况
+				CASE WHEN xtype IN (48,52,56,59,60,62,106,108,122,127) THEN '''0'')' ELSE ''' '')' END + --数据类型处理，对数值类型的，ISNULL给0，非数值的暂时给' '，以后出现转换失败的再进行处理
+				CASE WHEN xtype = 61 then ',121)' ELSE '' END 
+			  +' AS [TD],'+'+'''+''''
+	FROM tempdb..syscolumns WHERe id = OBJECT_ID(@TBNAME) AND name <>  'tmp_html_id'
+	order by colid
+	FOR XML PATH('')
+) 
+
+select @COLUMNS = STUFF(@COLUMNS,1,1,'')
  
-/*HTML����ʽ*/
+/*HTML表格式*/--border="1px"
 SELECT @SQLSTR = N'<H1 align = "center">'+@Mcontent+' </H1> 
-							<table border="1px" width = "900" cellspacing="0px" 
-								style="border-collapse:collapse;table-layout:fixed;font-size:14px;font-family:΢���ź�;white-space:pre-line"
-								cellpadding="0"; align = "center"   > <tr bgcolor="Silver" align = "center"  >'
+							<table  width = "1000" cellspacing="0px" frame="hsides"
+								style="border-collapse:collapse;table-layout:fixed;font-size:14px;font-family:微软雅黑;white-space:pre-line"
+								cellpadding="0"; align = "center"   > 								
+								<tr style="word-break : break-all;height:42" bgcolor = "#70ad47" align="center" >'
 
-/*��ͷ*/
-SELECT @SQLSTR = @SQLSTR+ CAST((SELECT name as 'td',+' '  FROM tempdb..syscolumns WHERe id = OBJECT_ID(@TBNAME) ORDER BY colid for XML PATH(''))AS NVARCHAR(MAX)) +'</tr>'
+/*表头*/
+SELECT @SQLSTR = @SQLSTR+ CAST((SELECT N'《font color="white"》'+name+'《/font》' as 'td',+''  FROM tempdb..syscolumns WHERe id = OBJECT_ID(@TBNAME) AND name<> 'tmp_html_id' ORDER BY colid for XML PATH(''))AS NVARCHAR(MAX)) +'</tr>'
+--SELECT @SQLSTR = @SQLSTR+ CAST((SELECT  name as 'td',+''  FROM tempdb..syscolumns WHERe id = OBJECT_ID(@TBNAME) AND name<> 'tmp_html_id' ORDER BY colid for XML PATH(''))AS NVARCHAR(MAX)) +'</tr>'
 
-/*ȡ�����ݵ��ַ���������ƴ��*/
+SELECT @SQLSTR = REPLACE(REPLACE(@SQLSTR,'《','<'),'》','>')
+--SELECT @SQLSTR
+/*取表数据的字符串，用来拼接*/
 SELECT @COLUMNS = N'SELECT @COLUMNS = CONVERT(NVARCHAR(MAX),
-					(SELECT 
-						 ''word-break : break-all; width="100%";'' as [@style],
-					     ''center'' as [@align],
-						  '+ @COLUMNS+ '   FROM '+@TBNAME+ ' FOR XML PATH('+'''tr''),type)) ' 
+					(SELECT '
+						  +'''center'' as [@align],'
+						  +'[@style] = '+'''word-break : break-all;height:42;background-color: ''+'+'CASE WHEN A.tmp_html_id%2=0 THEN '+'''#f4f1f4'''+ ' ELSE '+'''#FBFFFD'''+' END'+ ','
+						  + @COLUMNS
+						  + '  FROM '+@TBNAME+ ' AS A ORDER BY '+@ORDER_COLUMNS+' FOR XML PATH(''tr''),type))' 
+
+
  
-/*���������ݲ��뵽������ַ�����*/
+/*将表内数据插入到结果集字符串中*/
 exec sys.sp_executesql @COLUMNS,N'@TBNAME VARCHAR(100), @COLUMNS nVARCHAR(MAX) OUTPUT',@TBNAME, @COLUMNS OUTPUT
 
-/*ƴ���ַ��������*/ 
+/*拼接字符串结果集*/ 
 SELECT @SQLSTR=@SQLSTR+ @COLUMNS+ N'</table>'
 
-
---select @SQLSTR
-----select @tableHTML
+--SELECT @SQLSTR
+ 
 exec msdb.dbo.sp_send_dbmail
 	 @profile_name	= @profile_name, --@profile_name
-	 @recipients	= @recipients, --'funicia@xxx.com;zhengyue@xxx.com', 
+	 @recipients	= @recipients, --'renfc@gildata.com;zhengyue@gildata.com', 
 	 @copy_recipients = @copy_recipients,
 	 @subject		= @Title,
 	 @body			= @SQLSTR,
